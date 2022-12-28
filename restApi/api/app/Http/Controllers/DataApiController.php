6aecf6088e4b86f2c01f64521d86a4c41ba9380c
AsadSheikhJ <sheikhasad1652@gmail.com>
2022-12-28 16:39:03 +0500

    Final Optimizing State

--- restApi/api/app/Http/Controllers/DataApiController.php
+++ restApi/api/app/Http/Controllers/DataApiController.php
@@ -31,6 +31,7 @@ class DataApiController extends BaseController
         return response()->json('Have a great day!');
     }
 
+    // FireBase json Insertion Function
     public function DataEntry(Request $request, $route)
     {
         // here $route will contain this value "whatever/comes/here/1"
@@ -101,14 +102,15 @@ class DataApiController extends BaseController
             $primaryKeylocal = $primaryKeylocal[0]->Column_name;
             
             //Checks if provided Key matches
-            return ( $primaryKey != $primaryKeylocal && !empty($primaryKey) ) ? 
-            'provided key doesn\'t match with actual key...' : $primaryKeylocal ;
+            return response()->json(( $primaryKey != $primaryKeylocal && !empty($primaryKey) ) ? 
+            'provided key doesn\'t match with actual key...' : $primaryKeylocal );
 
         } catch (\Exception $e){
-            return ('Unable to find id from table to insert with...');
+            return response()->json('Unable to find id from table to insert with...');
         }
     }
 
+    // Simple json Insertion Function
     public function DataEntry_v2(Request $request, $route)
     {
         // here $route will contain this value "whatever/comes/here/1"
@@ -133,45 +135,47 @@ class DataApiController extends BaseController
                 Please check your configuration. error:" . $e->getMessage());
         }
 
-        // Test if the provided Table Exist in DataBase.
-        if (!Schema::hasTable($crfDB)) {
-            fwrite($file, response()->json($data) . "\n\n");
-            fclose($file);
+        // // Test if the provided Table Exist in DataBase.
+        // if (!Schema::hasTable($crfDB)) {
+        //     fwrite($file, response()->json($data) . "\n\n");
+        //     fclose($file);
 
-            return response()->json('the specified table doesn\'t exist..  
-                -- given table: ' . $crfDB);
-        }
+        //     return response()->json('the specified table doesn\'t exist..  
+        //         -- given table: ' . $crfDB);
+        // }
 
-        $primaryKey = $this->PrimaryKeyCheck($crfDB, request()->segment(4));
+        // Getting the primary key from table for data insertion..
+        // $primaryKey = $this->PrimaryKeyCheck($crfDB, request()->segment(4));
+        return $this->PrimaryKeyCheck($crfDB, request()->segment(4));
 
-        try{
-            // Objectifing the Array to Insert in Table
-            $objectifiedData = $DeviceApi_model->RawDataToObject($crfDB, $data);
-            unset($objectifiedData[$primaryKey]);
-        }catch(\Exception $e){
-            $msg['error']   = $e->getMessage(); 
-            $msg['status']  = "Failed, Empty dataset...";
-            $msg['code']    = 409;
-            return ($msg);
-        }
+        // try{
+        //     // Objectifing the Array to Insert in Table
+        //     $objectifiedData = $DeviceApi_model->RawDataToObject($crfDB, $data);
+        //     unset($objectifiedData[$primaryKey]);
+        // }catch(\Exception $e){
+        //     $msg['error']   = $e->getMessage(); 
+        //     $msg['status']  = "Failed, Empty dataset...";
+        //     $msg['code']    = 409;
+        //     return ($msg);
+        // }
 
-        // Inserting the objectified data
-        if ($objectifiedData) {
-            try{
-                // Function (Table Name(Required), DataID(Required), User Name, Data Timestamp, Objectified Data(Required), Primary Key(Required) )
-                $msg = $DeviceApi_model->DataEntryModel($crfDB, $data->$primaryKey, "Unknown" , null , $objectifiedData, $primaryKey);
-            } catch(\Exception $e){
-                fwrite($file,  $e->getMessage()."\n".response()->json($data) ."\n\n");
-                $msg['error']   = $e->getMessage();
-                $msg['status']  = "Failed";
-                $msg['code']    = 409;
-            }
-        } else {
-            $msg['status'] = "Failed, Empty dataset...";
-            $msg['code'] = 409;
-        }
+        // // Inserting the objectified data
+        // if ($objectifiedData) {
+        //     try{
+        //         // Function (Table Name(Required), DataID(Required), User Name, Data Timestamp, Objectified Data(Required), Primary Key(Required) )
+        //         $msg = $DeviceApi_model->DataEntryModel($crfDB, $data->$primaryKey, "Unknown" , null , $objectifiedData, $primaryKey);
+        //     } catch(\Exception $e){
+        //         fwrite($file,  $e->getMessage()."\n".response()->json($data) ."\n\n");
+        //         $msg['error']   = $e->getMessage();
+        //         $msg['status']  = "Failed";
+        //         $msg['code']    = 409;
+        //     }
+        // } else {
+        //     $msg['status'] = "Failed, Empty dataset...";
+        //     $msg['code'] = 409;
+        // }
 
-        // http_response_code($msg['code']);
-        return ($msg);
+        // // http_response_code($msg['code']);
+        // return ($msg);
     }
 }
